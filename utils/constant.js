ea82d5e4a43f514c4331d7368b1d1df818fb3697
zeroro1 <zeroro1@users.noreply.github.com>
2026-07-21 21:10:24 +0800

    feat: 分类选择器增加默认值'请选择'

--- utils/constant.js
+++ utils/constant.js
@@ -9,5 +9,5 @@ export const TYPE_MAP = {
   3: { label: '转账', color: '#1890ff' }
 }
 
-export const EXPENSE_CATEGORIES = ['餐饮', '交通', '购物', '住房', '娱乐', '医疗', '教育', '通讯', '日用', '其他']
-export const INCOME_CATEGORIES = ['工资', '奖金', '兼职', '理财', '红包', '退款', '其他']
+export const EXPENSE_CATEGORIES = ['请选择', '餐饮', '交通', '购物', '住房', '娱乐', '医疗', '教育', '通讯', '日用', '其他']
+export const INCOME_CATEGORIES = ['请选择', '工资', '奖金', '兼职', '理财', '红包', '退款', '其他']
